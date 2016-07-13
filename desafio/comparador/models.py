a4c2b9118ca051f425188edfc20388e6f9fb5947
Thiago <thiago.ar17@gmail.com>
2016-07-13 02:57:38 -0300

    Criando entidade Servidor

--- desafio/comparador/models.py
+++ desafio/comparador/models.py
@@ -3,3 +3,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Servidor(models.Model):
+    """
+    Classe responsavel por manter os dados cadastrais dos servidores
+    """
+
+    nome = models.CharField(verbose_name=u'Nome', max_length=100)
+    quantidade_cpu = models.IntegerField(verbose_name=u'Quantidade de CPU',)
+    quantidade_memoria = models.IntegerField(verbose_name=u'Quantidade memoria')
+    quantidade_hd = models.IntegerField(verbose_name=u'Quantidade de HD')
+    sistema_operacional = models.CharField(verbose_name=u'Sistema Operacional', max_length=200)
+    preco = models.FloatField(verbose_name=u'Preco')
+
+    class Meta:
+        db_table = u'servidor'
+
+    def __unicode__(self):
+        return self.nome
--- desafio/comparador/tests.py
+++ desafio/comparador/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from comparador.models import Servidor
+
 
 # Create your tests here.
+
+
+class ServidorTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_create_servidor_00(self):
+        Servidor.objects.create(nome="Servidor TESTE", quantidade_cpu=4, quantidade_memoria=1024,
+                                quantidade_hd=200, sistema_operacional='Linux', preco=100.0)
+        servidor = Servidor.objects.get(nome='Servidor TESTE')
+        self.assertEqual(servidor.quantidade_cpu, 4)
+        self.assertEqual(servidor.quantidade_hd, 200)
+        self.assertEqual(servidor.quantidade_memoria, 1024)
+        self.assertEqual(servidor.sistema_operacional, 'Linux')
+        self.assertEqual(servidor.preco, 100.00)
