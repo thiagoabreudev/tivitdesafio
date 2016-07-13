1b0047d69032ba3c91656c76c1a3e90a7dfeb9cc
Thiago <thiago.ar17@gmail.com>
2016-07-13 03:10:59 -0300

    Criando serializacao para entidade Servidor

--- desafio/comparador/serializers.py
+++ desafio/comparador/serializers.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
+from rest_framework import serializers
+from comparador.models import Servidor
+
+
+class ServidorSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Classe responsavel pela representacao dos dados da entidade Servidor
+    """
+    class Meta:
+        model = Servidor
+        fields = ('nome', 'quantidade_cpu', 'quantidade_memoria', 'quantidade_hd',
+                  'sistema_operacional', 'preco')
--- desafio/comparador/views.py
+++ desafio/comparador/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from comparador.models import Servidor
+from comparador.serializers import ServidorSerializer
 # Create your views here.
+
+
+class ServidorViewSet(viewsets.ModelViewSet):
+    """
+    Classe responsavel pela representacao da view da representacao ServidorSerializer
+    """
+    queryset = Servidor.objects.all()
+    serializer_class = ServidorSerializer
